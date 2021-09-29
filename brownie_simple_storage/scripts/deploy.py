2c52e4f68dcce233b4ee619510bc716e35010cc9
Simone Sangeniti <snakenextgen@gmail.com>
2021-09-29 18:51:18 +0200

    brownie update

--- brownie_simple_storage/scripts/deploy.py
+++ brownie_simple_storage/scripts/deploy.py
@@ -1,10 +1,39 @@
-from brownie import accounts, SimpleStorage
+from brownie import accounts, SimpleStorage, config, network
 
+account = ''
+contract = ''
 
 def deploy_simple_storage():
-    account = accounts[0]
-    SimpleStorage.deploy({'from': account})
+    global account, contract
+
+    # work with local ganache account
+    # account = accounts[0]
+
+    # work with testnet/mainnet using metamask account
+    # loading the addedd account
+    # account = accounts.load('testing-account')
+    # or load it from yaml file
+    
+    # account = accounts.add(config["wallets"]["from_key"])
+    # contract = SimpleStorage.deploy({'from': account})
+
+    account = get_account()
+    contract = SimpleStorage.deploy({'from': account})
+
+
+def interact_with_contract():
+    contract.store(15, {"from": account})
+    print(contract.retrieve())
+
+
+def get_account():
+    if network.show_active() == "development":
+        return accounts[0]
+    else:
+        accounts.add(config["wallets"]["from_key"])
 
 
 def main():
     deploy_simple_storage()
+
+    interact_with_contract()
