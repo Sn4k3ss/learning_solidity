3d007d4b3a1b03a61abd3250ab6e588bff497bea
Simone Sangeniti <snakenextgen@gmail.com>
2021-10-12 15:21:10 +0200

    Added standard ERC20 implementation from OpenZepp
    Token deployed successfully to Ethereum (Rinkeby) and Polygon (Mumbai)

--- my_token/scripts/deploy_token.py
+++ my_token/scripts/deploy_token.py
@@ -1,7 +1,19 @@
-from brownie import PoliToken, accounts, network, config
-from my_token.scripts.helpful_scripts import *
+from brownie import PoliToken, Token, accounts, network, config
+from scripts.helpful_scripts import *
+
+
+def deply_token():
+    account = get_account()
+
+    PoliToken.deploy(1e21, {"from": account})
+    #  Token.deploy("PoliToken", "POLI", 8, 1e11, {"from": account})
+
+
+def verify_token():
+    token = PoliToken[-1]
+    PoliToken.publish_source(token)
 
 
 def main():
-    account = get_account(index=0)
-    PoliToken.deploy(1e21, {"from": account})
+    deply_token()
+    verify_token()
